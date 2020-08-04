24ed26bcb735266c94913eb9a07e3ad49c382021
vinaypatel9998 <patelbhai9998@gmail.com>
2020-08-04 19:01:01 -0400

    loading effect

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
+	
+	 var x= function(){
+    document.getElementById("siteLoading").style.display="none";
+    }
     (function ($) {
     "use strict"; // Start of use strict
 
